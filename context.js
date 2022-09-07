6bd46ab06940c074425f7983b760a442e86a0d70
Asad-MA <asad.nextstair@gmail.com>
2022-09-07 19:17:08 +0500

    nex local

---
+++ context.js
@@ -0,0 +1,16 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '9900d5605amsh9274b8a92d23cf4p14e159jsn52d1762d9be5',
+		'X-RapidAPI-Host': 'cricbuzz-cricket.p.rapidapi.com'
+	}
+};
+
+function fetchRawData(){
+	fetch('https://cricbuzz-cricket.p.rapidapi.com/matches/v1/live', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+}
+
+fetchRawData();
