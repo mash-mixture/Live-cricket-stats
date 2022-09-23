d5acf539bda2b5116f8912441ed2bd4ea1cd6780
mash-mixture <asadorasoo32@gmail.com>
2022-09-23 21:24:49 +0500

    fixes

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,16 +1,16 @@
 //**** GLOBAL VARIABLES */
-var tabWrapper = document.querySelector('.tab-wrapper');
-var tab = tabWrapper.querySelectorAll('button');
+// var tabWrapper = document.querySelector('.tab-wrapper');
+// var tab = tabWrapper.querySelectorAll('button');
 
-tab.forEach((ele , index)=>{
-	ele.addEventListener('click',function(){
-		console.log(this.className);
-		currentTab = document.querySelector('.active-tab');
-		if(this.classList.contains('active-tab')) return;
-		currentTab.classList.remove('active-tab');
-		this.classList.add('active-tab');
-	})
-});
+// tab.forEach((ele , index)=>{
+// 	ele.addEventListener('click',function(){
+// 		console.log(this.className);
+// 		currentTab = document.querySelector('.active-tab');
+// 		if(this.classList.contains('active-tab')) return;
+// 		currentTab.classList.remove('active-tab');
+// 		this.classList.add('active-tab');
+// 	})
+// });
 
 
 
