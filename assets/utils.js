c1940d0d8d19c5f1942d8d405daad6e68eb58a3e
mash-mixture <asadorasoo32@gmail.com>
2022-10-01 10:07:40 +0500

    sat

--- assets/utils.js
+++ assets/utils.js
@@ -10,17 +10,31 @@ $('.recent-match-0-grid').slick({
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToShow: 2,
+          slidesToScroll: 1,
           infinite: true,
           dots: true
         }
       },
       {
-        breakpoint: 600,
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 767,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
         }
       },
       {
